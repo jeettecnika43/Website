c9ebd217d55e7f353d2dd81f70b994f93dbbcd52
jeettecnika43 <jeettecnika43@gmail.com>
2025-08-10 16:51:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple scroll animation for navigation clicks
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
